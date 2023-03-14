69e385b003d56a1ea00ce9a067b0641e7305afd7
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-03-13 20:10:27 -0400

    Begin scripting library behavior

---
+++ script.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book(name, author, pages, read) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function getBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        
+    }
+}
