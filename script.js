3283f7a480e585a0f7dd43c71d82c62fa546bea0
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-03-15 14:41:48 -0400

    Fix array pruning/dataset adjustment after removal

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const bookGrid = document.querySelector('#book-grid');
 
 //main
 let myLibrary = [];
+let counter = 0;
 
 function Book(name, author, pages, check) {
     this.name = name;
@@ -43,21 +44,27 @@ function populateBooks() {
     removeBtnCreate.classList.add('remove');
     removeBtnCreate.innerText = 'Remove?';
 
-    for (let i = 0; i < myLibrary.length; i++) {
-        bookCreate.innerText = `"${myLibrary[i].name}"`;
-        authorCreate.innerText = 'By: ' + myLibrary[i].author;
-        pagesCreate.innerText = myLibrary[i].pages + ' pages';
-        myLibrary[i].check == true ? checkCreate.innerText = "Have read" : checkCreate.innerText = "Haven't read";
+    while (counter < myLibrary.length) {
+        bookCreate.innerText = `"${myLibrary[counter].name}"`;
+        authorCreate.innerText = 'By: ' + myLibrary[counter].author;
+        pagesCreate.innerText = myLibrary[counter].pages + ' pages';
+        myLibrary[counter].check == true ? checkCreate.innerText = "Have read" : checkCreate.innerText = "Haven't read";
         cardCreate.append(bookCreate, authorCreate, pagesCreate, checkCreate, removeBtnCreate);
+        cardCreate.dataset.num = counter;
         bookGrid.appendChild(cardCreate);
+        counter++;
     }
 
-    const removeBtn = document.querySelectorAll('.remove');
-    
-    removeBtn.forEach(btn => {
-        btn.addEventListener('click', () => {
-            btn.parentElement.remove();
+    const remove = bookGrid.lastElementChild.lastElementChild;
+
+    remove.addEventListener('click', () => {
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            if (card.dataset.num >= remove.parentElement.dataset.num) card.dataset.num--;
         });
+        counter--;
+        remove.parentElement.remove();
+        myLibrary.splice(remove.parentElement.dataset.num, 1);
     });
 }
 
