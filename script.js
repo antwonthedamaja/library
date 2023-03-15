1d3b3e3870b1430460e83e753c95d4b9a164d201
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-03-14 20:03:03 -0400

    Fix transparency error with cards, add modal popup

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// modal
+const openModal = document.querySelector('.open-modal');
+const closeModal = document.querySelector('.close-modal');
+const modal = document.querySelector('#modal');
+
+openModal.addEventListener('click', () => {
+    modal.showModal();
+})
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+})
+
+//form listeners
+const submit = document.querySelector('.submit');
+
+
 let myLibrary = [];
 
 function Book(name, author, pages, read) {
@@ -13,6 +30,11 @@ function addBookToLibrary(book) {
 
 function getBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
-        
+
     }
 }
+
+submit.addEventListener('click', () => {
+    
+    modal.close();
+})
